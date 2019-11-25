b7ffd57d50c5c5e5ad4279fcb45e596ed1929152
L-Garay <garay.logan@gmail.com>
2019-11-25 10:37:55 -0700

    final

--- app/Models/List.js
+++ app/Models/List.js
@@ -9,25 +9,25 @@ export default class List {
   }
   get Template() {
     return `
-    <div class="col-12 col-sm-6 col-md-4 mt-2 p-2 list-cols">
-      
-        <h4 class="list-title">${
-          this.name
-        } <button class="btn btn-outline btn-sm list-btn ml-1" onclick="app.listController.removeList('${
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title list-title">${
+            this.name
+          } <button class="btn btn-outline btn-sm list-btn ml-1" onclick="app.listController.removeList('${
       this.id
     }')"><i class="far fa-times-circle fa-xs"></i></button></h4>
-        <form class="border-bottom" onsubmit="app.listController.addTask(event, '${
-          this.id
-        }')">
-        <div class="form-group">
+          <form class="border-bottom" onsubmit="app.listController.addTask(event, '${
+            this.id
+          }')">
+          <div class="form-group">
           <label for="name">New Task</label>
           <input type="text" class="form-control special-form" id="name" placeholder="Enter task" />
+          </div>
+          <button type="submit" class="btn submit-btn">Submit</button>
+          </form>
+          <dl class="ml-3 mt-2">${this.getTaskTemplates()}</dl>
         </div>
-        <button type="submit" class="btn submit-btn">Submit</button>
-        </form>
-        <dl class="ml-3 mt-2">${this.getTaskTemplates()}</dl>
-      
-    </div>
+      </div>
           `;
   }
 
